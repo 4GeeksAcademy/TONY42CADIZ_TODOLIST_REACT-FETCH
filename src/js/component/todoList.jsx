0eddc11731fa6bee521bc653ae29c65519877e0e
ANTONIO MANUEL MOLINA CAÑETE <MOLFERGUELSA@HOTMAIL.COM>
2025-01-24 15:40:23 +0000

    Todolist_react-fetch

---
+++ src/js/component/todoList.jsx
@@ -0,0 +1,168 @@
+import React, { useEffect, useState } from "react";
+
+const TodoList = ( ) => {
+    const [input, setInput] = useState ("")
+    const [tasks, setTasks] = useState ([])
+    const [hovered, setHovered] = useState (null)
+
+  
+    function getTask() {
+        console.log("carica task");
+    
+        fetch('https://playground.4geeks.com/todo/users/tony42cadiz')
+        .then((response) => response.json())
+        .then((data) => setTasks(data.todos))
+    } 
+    useEffect(()=>{
+    console.log("componente cargado!");
+    getTask()
+    },[])  
+
+function addTask (e) {
+    console.log("añade task");
+
+    if (e.key === "Enter"){
+    const requestOption = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            "label": input,                
+            "is_done": false
+        })
+    }
+
+    fetch('https://playground.4geeks.com/todo/todos/tony42cadiz', requestOption)
+    .then((response) => response.json())
+    .then((data) => { 
+    getTask();                         
+    setInput("");
+        }
+    )
+}
+}
+function removeTask(id) {
+    console.log("borra task", id);
+
+             
+
+    const requestOption2 = {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json"},
+    }
+        fetch('https://playground.4geeks.com/todo/todos/'+ id, requestOption2)
+        .then((response) => response.text())
+        .then((data)=> getTask())                 
+
+}
+
+function showX(index) {
+    setHovered(index);   
+}
+
+function hideX(){
+    setHovered(null);
+}
+
+function deleteAllTasks() {
+    console.log("borra TODAS las tasks");
+    const deleteTasks = tasks.map((item) => {
+    const requestOption3 = {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json"},
+    }
+       return fetch("https://playground.4geeks.com/todo/todos/"+ item.id, requestOption3)
+    })
+        Promise.all(deleteTasks)
+        .then(()=>{
+        setTasks([]);
+    });
+}      
+  
+    return (
+        <>
+        <div className="container" style={{border: "0", height: "750px", width: "800px", backgroundColor: "rgb(243, 243, 243)", marginTop: "25px"}}>
+          <h1 style={{textAlign: "center", fontSize: "105px", fontWeight: "100", marginTop: "30px", color: "rgb(236, 220, 221)", fontFamily: "Roboto", paddingTop: "10px"}}>
+            Tasks
+          </h1>
+          <ul style={{listStyle: "none", position: "relative", zIndex: "2", marginRight: "80px"}}>
+            <li>
+              <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={addTask}
+                className="form-control"
+                id="input"
+                placeholder="What needs to be done?"
+                style={{
+                  fontFamily: "Roboto", fontWeight: "360", fontSize: "26px", color: "rgb(108, 108, 108)", 
+                  boxShadow: "4px 0px 10px rgba(0, 0, 0, 0.1)", border: "solid rgb(221, 221, 221) 1px", 
+                  borderBottomWidth: "inherit", marginTop: "0px", width: "600px", height: "65px", margin: "0 auto", 
+                  borderRadius: "0px", backgroundColor: "rgb(249, 249, 249)", paddingLeft: "60px"
+                }}
+              />
+            </li>
+            {tasks.length === 0 ? (
+              <li style={{
+                fontFamily: "Roboto", fontWeight: "360", fontSize: "24px", color: "lightgrey", 
+                boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.1)", backgroundColor: "rgb(249, 249, 249)", 
+                border: "solid rgb(221, 221, 221) 1px", borderBottomWidth: "inherit", marginTop: "0px", 
+                width: "600px", height: "65px", margin: "0 auto", display: "flex", alignItems: "center", 
+                justifyContent: "space-between", paddingLeft: "180px", paddingRight: "10px"
+              }}>
+                No tasks, add a task
+              </li>
+            ) : (
+              tasks.map((item, index) => (
+                <li key={item.id} style={{
+                  fontFamily: "Roboto", fontWeight: "360", fontSize: "26px", backgroundColor: "rgb(249, 249, 249)", 
+                  border: "solid rgb(221, 221, 221) 1px", marginTop: "0px", width: "600px", height: "65px", margin: "0 auto", 
+                  display: "flex", alignItems: "center", justifyContent: "space-between", paddingLeft: "60px", 
+                  paddingRight: "10px", fontSize: "20px"
+                }} 
+                  onMouseEnter={() => showX(index)} onMouseLeave={hideX}
+                >
+                  {item.label} 
+                  {/* {hovered === index && ( */}
+                    <i className="fa-solid fa-x" 
+                      style={{cursor: "pointer", color: "red", paddingRight: "10px", display: hovered===index ? "block":"none"}}
+                      onClick={() => removeTask(item.id)}
+                    ></i>
+                  {/* )} */}
+                </li>
+              ))
+            )}
+            <p style={{
+              fontFamily: "Roboto", fontWeight: "360", fontSize: "15px", color: "gray", 
+              boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.1)", backgroundColor: "rgb(249, 249, 249)", 
+              border: "solid rgb(221, 221, 221) 1px", marginTop: "0px", width: "600px", height: "45px", 
+              margin: "0 auto", display: "flex", alignItems: "center", paddingLeft: "10px"
+            }}>
+              {tasks.length} tasks left
+            </p>
+            <div style={{
+              border: "solid rgb(221, 221, 221) 1px", height: "7px", width: "594px", 
+              boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.1)", backgroundColor: "rgb(249, 249, 249)", 
+              marginTop: "5px", zIndex: "1", position: "absolute", left: "67px", bottom: "-7px"
+            }}></div>
+            <div style={{
+              border: "solid rgb(221, 221, 221) 1px", height: "9px", width: "585px", 
+              boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.2)", backgroundColor: "rgb(249, 249, 249)", 
+              marginTop: "5px", zIndex: "0", position: "absolute", left: "72px", bottom: "-13px"
+            }}></div>
+          </ul>
+        </div>
+        <button 
+          onClick={deleteAllTasks} 
+          style={{
+            height: "50px", width: "180px", backgroundColor: "rgb(249, 249, 249)", marginLeft: "70px", 
+            border: "solid rgb(221, 221, 221) 1px", fontFamily: "Roboto", fontWeight: "360", fontSize: "15px", 
+            color: "red", fontWeight: "inherit", boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.1)", borderRadius: "25px"
+          }}
+        >
+          <i className="fa-solid fa-trash"></i> ALL TASKS
+        </button>
+      </>
+    );
+  }
+export default TodoList
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,9 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
+import Todolist from "./component/todoList.jsx";
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<Todolist />)
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
 
